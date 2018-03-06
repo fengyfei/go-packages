88eb340c62fb34cd63f629452c9f9512663d840b
Feng Yifei <fengyfei@gmail.com>
2018-03-06 10:01:03 +0800

    add test case

--- queue/queue_test.go
+++ queue/queue_test.go
@@ -1,24 +1,34 @@
 package queue
 
 import (
-	"testing"
 	"fmt"
+	"testing"
 )
 
 func TestQueue(t *testing.T) {
 	q := NewQueue(6)
 
-	for i := 0; i < 35; i++ {
-		q.Put(fmt.Sprintf("%d",i))
+	for i := 0; i < 12; i++ {
+		q.Put(fmt.Sprintf("%d", i))
 	}
 
-	for i := 0; i < 35; i++ {
-		if q.Get() != fmt.Sprintf("%d",i) {
+	for i := 0; i < 12; i++ {
+		if q.Get() != fmt.Sprintf("%d", i) {
 			t.Fatal("get element err")
 		}
 	}
 
 	if q.Get() != nil {
 		t.Fatal("get unwanted element")
+	}
+
+	for i := 0; i < 12; i++ {
+		q.Put(fmt.Sprintf("%d", i))
+	}
+
+	for i := 0; i < 12; i++ {
+		if q.Get() != fmt.Sprintf("%d", i) {
+			t.Fatal("get element err")
+		}
 	}
 }
